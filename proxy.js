39dc9d6ff879ae5d3df9eede4c0abb06f5a9dfab
Jennifer_Barbosa_Alvarez <utm21040150@utma.edu.mx>
2026-03-01 09:54:51 -0600

    Activando página principal

--- proxy.js
+++ proxy.js
@@ -73,4 +73,4 @@ app.post('/log', async (req, res) => {
 
 // 🔥 Puerto dinámico obligatorio en Railway
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));git add .
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
