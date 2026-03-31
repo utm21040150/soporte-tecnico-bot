53fbcead15953b8da3cf2cebf9c4319271341610
Jennifer_Barbosa_Alvarez <utm21040150@utma.edu.mx>
2026-03-30 19:11:56 -0600

    Asignacion de tickets_7

--- bot.js
+++ bot.js
@@ -361,7 +361,15 @@ A continuación te haremos una breve encuesta para generar tu ticket.
             case 4:
                 s.data.ubicacion = msg.body;
                 s.data.id = "SRV-" + Date.now();
-                s.data.fecha = new Date().toLocaleString();
+                s.data.fecha = new Date().toLocaleString('es-MX', {
+                    timeZone: 'America/Mexico_City',
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit'
+                  });
 
                 // CORRECCIÓN: Usar valores por defecto si no existen
                 const problemaDesc = s.data.problema_descripcion || s.data.problema || "No especificado";
